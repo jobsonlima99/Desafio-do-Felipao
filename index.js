5b2ce3f9fb77d48b6dfc2055efac5d034912c1dc
jobsonlima99 <jobson.anjos9@gmail.com>
2024-05-24 22:19:21 -0400

    Fiz um update no codigo, depois de pensar baste nas opções. Usei a estrutura de if, else if, else e switch

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 let nomeDoHeroi = "Chapolin"
-let nivelXpDoHeroi = 900
+let nivelXpDoHeroi = 11000
 let hankDoHeroi = ""
 
-switch (nivelXpDoHeroi){
-    case nivelXpDoHeroi <= 1000:
-    hankDoHeroi = "Ferro"
-    console.log("O Heroi de nome " + nomeDoHeroi + " está no nivel de " + hankDoHeroi)
-    break
+if (nivelXpDoHeroi >= 10001 ){
+    
+    hankDoHeroi = "Radiante"
 
-    case nivelXpDoHeroi >= 2000:
+}else if(nivelXpDoHeroi >= 10000 ){
+    
+    hankDoHeroi = "Imoortal"
+    
+}else if(nivelXpDoHeroi >= 9000 ){
+    
+    hankDoHeroi = "Ascendente"
+    
+}else if(nivelXpDoHeroi >= 8000 ){
+    hankDoHeroi = "Platina"
+    
+}else if(nivelXpDoHeroi >= 7000 ){
+    
+    hankDoHeroi = "Ouro"
+    
+}else if(nivelXpDoHeroi >= 5000 ){
+    
+    hankDoHeroi = "Prata"
+    
+}else if(nivelXpDoHeroi >= 2000 ){
+    
     hankDoHeroi = "Bronze"
-    console.log("O Heroi de nome " + nomeDoHeroi + " está no nivel de " + hankDoHeroi)
+    
+}else{
+    
+    hankDoHeroi = "Ferro"
+    
+}
+
+switch(hankDoHeroi){
+    case "Ferro":
+    case "Bronze":
+    case "Prata":
+    case "Ouro":
+    case "Platina":
+    case "Ascendente":
+    case "Imortal":
+    case "Radiante":
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + hankDoHeroi)
     break
 }
